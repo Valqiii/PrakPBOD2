f63ecdcf24f734904c62710c221b497a0e702e44
Valqiii <avaldirifqi@gmail.com>
2025-04-30 09:20:28 +0700

    Add files via upload

---
+++ Meet 7 Post test 9b/Anjing.java
@@ -0,0 +1,11 @@
+public class Anjing extends Anabul {
+    @Override
+    public void bersuara() {
+        System.out.println("Anjing: Guk-guk");
+    }
+
+    @Override
+    public void bergerak() {
+        System.out.println("Anjing melata.");
+    }
+}
