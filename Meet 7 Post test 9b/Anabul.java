c231b834ab28129f59c8fe55788d3101c8ceed01
Valqiii <avaldirifqi@gmail.com>
2025-04-30 09:22:14 +0700

    Delete Meet 7 Post test 9b directory

--- Meet 7 Post test 9b/Anabul.java
+++
@@ -1,9 +0,0 @@
-public class Anabul {
-    public void bersuara() {
-        System.out.println("Anabul bersuara.");
-    }
-
-    public void bergerak() {
-        System.out.println("Anabul bergerak.");
-    }
-}
